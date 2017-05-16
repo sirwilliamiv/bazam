5d378de15b564989bf427ebbf18d4833370e39a3
sirwilliamiv <wjconnolly2@gmail.com>
2017-05-16 15:33:33 -0500

    readded findsong execute inside record function

--- bazam/www/app/controllers/callCtrl.js
+++ bazam/www/app/controllers/callCtrl.js
@@ -6,16 +6,17 @@ bazam.controller('callCtrl',function($scope, $ionicLoading, songFactory,$window)
 const recorder = new Object;
 
 recorder.record = function() {
-  alert("recording, before")
+  // alert("recording, before")
   $scope.message1 ="recordingsample,before"
   window.plugins.audioRecorderAPI.record( function(msg) {
     $scope.message1 ="recordingsample 7 seconds"
     // complete
-    alert('ok: ' + msg);
+    alert('success: ' + msg);
+    $scope.findSong(msg)
   }, function(msg) {
        $scope.message1 ="recording failed"
     // failed
-    alert('ko: ' + msg);
+    alert('fail: ' + msg);
   }, 7); // record 7 seconds
 }
 
@@ -23,7 +24,7 @@ recorder.record = function() {
     console.log("1/5")
     $scope.message1 ="starting"
     let things = "stuff"
-    alert("starting")
+    // alert("starting")
     // songFactory.identify(things)
     recorder.record()
     console.log('recording!!!')
@@ -32,7 +33,7 @@ recorder.record = function() {
   $scope.findSong = (cb) => {
     console.log("4/5")
     $scope.message1 ="findingSong"
-    alert("calling songfactory")
+    // alert("calling songfactory")
     songFactory.identify(cb)
     .then((res) => {
       console.log("5/5")
