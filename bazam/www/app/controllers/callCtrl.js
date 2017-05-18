50ea1a4f009498845aa7e47826427da9444b1417
sirwilliamiv <wjconnolly2@gmail.com>
2017-05-18 18:14:24 -0500

    put in older version of cordova api from web

--- bazam/www/app/controllers/callCtrl.js
+++ bazam/www/app/controllers/callCtrl.js
@@ -6,20 +6,22 @@ bazam.controller('callCtrl', function($scope, $ionicLoading, songFactory, $windo
   const recorder = new Object;
 
   recorder.record = function() {
-    // alert("recording, before")
-    $scope.message1 = "recordingsample,before"
-    window.plugins.audioRecorderAPI.record(function(data) {
-      // success
-      $scope.findSong(data)
-    }, function(data) {
-      // failed
-      alert('fail: ' + msg);
-    }, 5); // record 7 seconds
-  }
+      window.plugins.audioRecorderAPI.record(function(data) {
+        // success
+        $scope.findSong(data)
+          // $scope.arrayBufferToBase64(data)
+      }, function(data) {
+        // failed
+        alert('fail: ' + msg);
+      }, 5); // record 7 seconds
+    } //end recorder.record
 
   $scope.bazam = () => {
     recorder.record()
+
   }
+
+
 
   $scope.findSong = (data) => {
 
@@ -27,19 +29,25 @@ bazam.controller('callCtrl', function($scope, $ionicLoading, songFactory, $windo
         .then((base64toPost) => {
           // alert("convert resolve base?"+ base64toPost)
           // up to here works
+        //   return $scope.message1 = base64toPost
+        // }).catch((err) => {
+        //   alert(err)
+        // })
+
           return songFactory.solvetheproblem(base64toPost)
             .then((songFound) => {
               // alert("solvetheproblem resolve")
-              return $scope.message1 = songFound
+              return $scope.message1 = songFound.data
             })
             .catch((err) => {
               return alert("songfound err" + err.message + err)
             })
         }).catch((err) => {
           // alert("ERROR IN SONGFACTOR.INDEITFY")
-          return alert("last catch in findsong  >  " + err.erorr +"   " + err.message)
+          return alert("last catch in findsong  >  " + err.erorr + "   " + err.message)
         })
-    }
+      }
+
     //things go here
     //  $scope.showLoading = function() {
     //    $ionicLoading.show({
--- bazam/www/app/factories/songFactory.js
+++ bazam/www/app/factories/songFactory.js
@@ -3,76 +3,100 @@ bazam.factory('songFactory', function($http) {
 
 
 
-    // module.exports = { ACR_ACCESS_ACCOUNT, ACR_SECRET_KEY, ACR_HOST }
-    //gets local audio file and converts to base64
-    //
-    //
-    const solvetheproblem = (send64) => {
-      // alert("send64"+ send64)
-       // const send64 = send64
-       let send64Obj = {
-        send64: send64
-       }
 
-      //INSERT HEROKU URL
-     return $http.post(`https://vast-cove-65313.herokuapp.com/api/v1/song/find`, send64Obj)
-        .then((data) => {
-          // alert("hey")
-          // alert(data)
-          if (data) {
-            alert("super success -->>>     " + data)
-            return data
-          }
-          if (data.status === 201) {
-            alert("song not found")
+  const solvetheproblem = (send64) => {
+    // alert("send64"+ send64)
+    // const send64 = send64
+    let send64Obj = {
+      send64: send64
+    }
 
-          } else {
-            $location.path('/login');
-          }
-        })
-        .catch((err) => {
-          // alert('ERRROORRR')
-          alert("eerrrrr from post in solvetheproblem" + err)
-          return (err);
-        });
-      // });
-    };
+    //INSERT HEROKU URL
+    return $http.post(`https://vast-cove-65313.herokuapp.com/api/v1/song/find`, send64Obj)
+      .then((data) => {
+        // alert("hey")
+        // alert(data)
+        if (data) {
+          alert("super success -->>>     " + data)
+          return data
+        }
+        if (data.status === 201) {
+          alert("song not found")
 
-  const convert = (url) => {
-    // alert("file conversion")
+        } else {
+          $location.path('/login');
+        }
+      })
+      .catch((err) => {
+        // alert('ERRROORRR')
+        alert("eerrrrr from post in solvetheproblem" + err)
+        return (err);
+      });
+    // });
+  };
 
-    return new Promise((resolve, reject) => {
+const convert = (url) => {
+alert("path?--->" + url)
+//gets local audio file and converts to base64
+  // }
+  //
+  //    // alert("file conversion")
+      // return new Promise((resolve,reject) => {
+      //   resolve(url)
+      // })
+      ///******************************
+      return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
-      xhr.responseType = 'blob';
-      xhr.onload = function() {
-          const reader  = new FileReader();
-         reader.readAsDataURL(xhr.response)
+        xhr.responseType = 'blob';
+        xhr.onload = function() {
+          const reader = new FileReader();
+          reader.readAsDataURL(xhr.response)
 
-         reader.onloadend = function () {
-              resolve(reader.result)
+          reader.onloadend = function() {
+            resolve(reader.result)
           }
-      };
-      xhr.open('GET', url);
-      xhr.send();
+        };
+        xhr.open('GET', url);
+        xhr.send();
         // resolve(xhr.response)
-            });
-            //
-          xhr.send();
-          } //end convert
+      });
+      //
+      xhr.send();
+    } //end convert
 
-        // }) //end promise
-
-      // }
-      // find:() => {
-      //posts base64 encoded file to be identified via acrcloud
-      // let identify = (msg) => {
-      //  return new Promise ((resolve, reject) => {
-      //       // alert("inside identify")
-      //   // console.log("identify", msg)
-      //       convert(msg, solvetheproblem )
-      // }//end identify
+  // }) //end  just let url pass thru promise
 
 
 
-    return { convert, solvetheproblem } //end return
-  })
+
+
+
+
+
+
+
+
+  return { convert, solvetheproblem } //end return
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // }
+  // find:() => {
+  //posts base64 encoded file to be identified via acrcloud
+  // let identify = (msg) => {
+  //  return new Promise ((resolve, reject) => {
+  //       // alert("inside identify")
+  //   // console.log("identify", msg)
+  //       convert(msg, solvetheproblem )
+  // }//end identify
